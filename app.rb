568b73b4c8310d2e1a8788d8309deb3ec08fb253
yassimortensen <yassimortensen@gmail.com>
2017-11-01 15:14:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get "/" do
+    erb :index
+  end
+
+  get "/new" do
+    erb :create_puppy
+  end
+
+  post "/" do
+    @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    # You'll need to have a third controller action that takes the input from the
+    # user and renders a third view (views/display_puppy.erb) which displays the info for the puppy that was just created.
+    erb :display_puppy
+  end
+
 end
